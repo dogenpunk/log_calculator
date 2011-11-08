f674c7624c7df4ec8195abc6d349d04d1671ecaf
dogenpunk <dogenpunk@gmail.com>
2011-11-08 11:11:26 -0600

    Add specs for hours spanning days

--- spec/hours_spec.rb
+++ spec/hours_spec.rb
@@ -4,6 +4,7 @@ describe LogCalculator::Hours do
   describe ".new" do
     context "with no arguments" do
       subject { LogCalculator::Hours.new }
+
       its(:start)   { should be_nil }
       its(:finish)  { should be_nil }
       its(:total)   { should be_nil }
@@ -11,6 +12,7 @@ describe LogCalculator::Hours do
 
     context "with a start time" do
       subject { LogCalculator::Hours.new(start: "2011-09-20 08:00:00 -0500") }
+
       its(:start)   { should == Time.parse("2011-09-20 08:00:00 -0500") }
       its(:finish)  { should be_nil }
       its(:total)   { should be_nil }
@@ -20,6 +22,7 @@ describe LogCalculator::Hours do
       subject { LogCalculator::Hours.new(start: "2011-09-20 08:00:00 -0500",
                                          finish: "2011-09-20 17:00:00 -0500")
       }
+
       its(:start)  { should == Time.parse("2011-09-20 08:00:00 -0500") }
       its(:finish) { should == Time.parse("2011-09-20 17:00:00 -0500") }
       its(:total)  { should == 9 }
@@ -31,6 +34,7 @@ describe LogCalculator::Hours do
       subject { LogCalculator::Hours.new(start: "2011-09-20 07:30:00 -0500",
                                          finish: "2011-09-20 12:00:00 -0500")
       }
+
       its(:total) { should == 4.5 }
       its(:regular_hours) { should == 4 }
       its(:emergency_hours) { should == 0.5 }
@@ -40,6 +44,7 @@ describe LogCalculator::Hours do
       subject { LogCalculator::Hours.new(start: "2011-09-20 12:00:00 -0500",
                                          finish: "2011-09-20 18:15:00 -0500")
       }
+
       its(:total) { should == 6.25 }
       its(:regular_hours) { should == 5 }
       its(:emergency_hours) { should == 1.25 }
@@ -49,6 +54,7 @@ describe LogCalculator::Hours do
       subject { LogCalculator::Hours.new(start: "2011-09-20 17:00:00 -0500",
                                          finish: "2011-09-20 18:45:00 -0500")
       }
+
       its(:total) { should == 1.75 }
       its(:regular_hours) { should == 0 }
       its(:emergency_hours) { should == 1.75 }
@@ -58,8 +64,29 @@ describe LogCalculator::Hours do
       subject { LogCalculator::Hours.new(start: "2011-09-20 23:00:00 -0500",
                                          finish: "2011-09-21 01:00:00 -0500")
       }
+
       its(:total) { should == 2.00 }
       its(:regular_hours) { should == 0 }
+      its(:emergency_hours) { should == 2.00 }
+    end
+
+    context "with regular hours spanning two different days" do
+      subject { LogCalculator::Hours.new(start: "2011-09-20 16:00:00 -0500",
+                                         finish: "2011-09-21 09:00:00 -0500")
+      }
+
+      its(:total) { should == 17.00 }
+      its(:regular_hours) { should == 2.00 }
+      its(:emergency_hours) { should == 15.00 }
+    end
+
+    context "with start time before 8AM and end time after 5PM" do
+      subject { LogCalculator::Hours.new(start: "2011-09-20 07:00:00 -0500",
+                                         finish: "2011-09-20 18:00:00 -0500")
+      }
+
+      its(:total) { should == 11.00 }
+      its(:regular_hours) { should == 9.00 }
       its(:emergency_hours) { should == 2.00 }
     end
   end
